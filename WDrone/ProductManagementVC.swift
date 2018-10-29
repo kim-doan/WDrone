256440d5c9eff9416d59a4675448f3559feaf076
김도안 <doan0105@naver.com>
2018-10-30 02:15:21 +0900

    2018/10/30 AM 2:15 - 조금 수정

--- WDrone/ProductManagementVC.swift
+++ WDrone/ProductManagementVC.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import Alamofire
 
 class ProductManagementVC: UITableViewController {
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     override func viewDidLoad() {
         let backBtn = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)));
         self.navigationItem.leftBarButtonItem = backBtn;
+        
+        let alamo = Alamofire.request("http://35.194.119.42:8080/productList", method: .get, encoding: URLEncoding.httpBody);
+        
+        alamo.responseString() { response in
+            print("post=\(response.result.value!)");
+            
+            if response.result.value! != "nil" { // 들어온 값이 "nil"이 아니면 값을 받는다 // nil == 데이터없음
+//                //post 토큰 나누기
+//                let infoToken = response.result.value!;
+//                let tokenArr = infoToken.components(separatedBy: "&");
+//                var tokenString = Array<String>();
+//                var i : Int = 0;
+//                for _ in i..<tokenArr.count {
+//                    tokenString.append(tokenArr[i]);
+//                }
+//                let productinfo = ProductInfoVO();
+//
+//                if tokenArr[0] == "1" { // header가 1이라면
+//                    productinfo.id = tokenArr[1]; // 아이디
+//                    productinfo.name = tokenArr[2]; // 이름
+//                    productinfo.amount = Int(tokenArr[3])!; // 수량
+//                    productinfo.input = tokenArr[4]; // 반입날짜
+//                    productinfo.output = tokenArr[5]; // 반출날짜
+//
+//                    print(productinfo.id);
+//                    // 앱 델리게이트 객체를 읽어온 다음, productlist 배열에 productinfo 객체를 추가한다.
+//                    self.appDelegate.productlist.append(productinfo);
+//                }
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
